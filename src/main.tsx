f38e8f5ae374184c0e234a8f54b281fefa43c789
AuraClient <brawlbosssowin@gmail.com>
2026-03-28 01:21:13 +0300

    add react-router and legal pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,24 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
+import Offer from './pages/Offer';
+import Privacy from './pages/Privacy';
+import Contacts from './pages/Contacts';
 import './index.css';
 import { ErrorBoundary } from './components/ErrorBoundary';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorBoundary>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/offer" element={<Offer />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/contacts" element={<Contacts />} />
+        </Routes>
+      </BrowserRouter>
     </ErrorBoundary>
   </StrictMode>,
 );
