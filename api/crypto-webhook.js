867306a3c051cb0fcca9d1aca77407334a14c169
AuraClient <brawlbosssowin@gmail.com>
2026-03-29 18:11:36 +0300

    fix: reduce vercel functions to hobby limit

--- api/crypto-webhook.js
+++
@@ -1,5 +0,0 @@
-﻿import paymentWebhookHandler from './payments/webhook.js';
-
-export default async function handler(req, res) {
-  return paymentWebhookHandler(req, res);
-}
--- api/payments/webhook.js
+++
@@ -1,5 +0,0 @@
-import yookassaWebhookHandler from './yookassa-webhook.js';
-
-export default async function handler(req, res) {
-  return yookassaWebhookHandler(req, res);
-}
